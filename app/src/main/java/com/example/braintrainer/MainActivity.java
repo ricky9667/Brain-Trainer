84a350ebfb805c577e05dae269559f40465ad7c1
Ricky Hu <ricky9667@gmail.com>
2020-03-08 12:48:50 +0800

    success

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.braintrainer;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,11 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView countDownTextView, problemTextView, scoreTextView, messageTextView;
     Button option0Button, option1Button, option2Button, option3Button, playAgainButton;
+    ArrayList<Integer> options = new ArrayList<>();
 
-    ArrayList<Integer> options = new ArrayList<Integer>();
+    int correct, answered, answerIndex;
     boolean gameOnProgress;
 
     public void onGoButtonPressed(View view) {
+
         Button goButton = (Button) view;
         goButton.setVisibility(View.GONE);
 
@@ -48,12 +51,11 @@ public class MainActivity extends AppCompatActivity {
         option1Button.setVisibility(View.VISIBLE);
         option2Button.setVisibility(View.VISIBLE);
         option3Button.setVisibility(View.VISIBLE);
-
     }
 
     public void startGame() {
 
-        new CountDownTimer(30000, 1000) {
+        new CountDownTimer(31000, 1000) {
 
             public void onTick(long millisLeft) {
                 gameOnProgress = true;
@@ -68,15 +70,23 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+        correct = 0;
+        answered = 0;
+        String scoreText = correct + "/" + answered;
+        scoreTextView.setText(scoreText);
+
         generateProblem();
         playAgainButton.setVisibility(View.INVISIBLE);
+        messageTextView.setVisibility(View.INVISIBLE);
     }
 
     public void generateProblem() {
 
         Random rand = new Random();
 
-        int answerIndex = rand.nextInt(4);
+        options.clear();
+
+        answerIndex = rand.nextInt(4);
         int a = rand.nextInt(20) + 1;
         int b = rand.nextInt(20) + 1;
 
@@ -84,10 +94,8 @@ public class MainActivity extends AppCompatActivity {
         problemTextView.setText(problemText);
 
         for (int i=0; i<4; i++) {
-
-            if (i == answerIndex) {
-                options.add(a+b);
-            } else {
+            if (i == answerIndex) options.add(a + b);
+            else {
                 options.add(rand.nextInt(40) + 1);
                 while (options.get(i) == a+b) {
                     options.set(i, rand.nextInt(40) + 1);
@@ -101,9 +109,55 @@ public class MainActivity extends AppCompatActivity {
         option3Button.setText(String.valueOf(options.get(3)));
     }
 
+    public void onOptionClicked(View view) {
+
+        if (gameOnProgress) {
+            int buttonIndex = Integer.parseInt(view.getTag().toString());
+
+            if(answerIndex == buttonIndex) {
+                correct++;
+                messageTextView.setText("Correct!");
+            } else {
+                messageTextView.setText("Wrong :(");
+            }
+            answered++;
+
+            String scoreText = correct + "/" + answered;
+            scoreTextView.setText(scoreText);
+
+            generateProblem();
+
+            messageTextView.setVisibility(View.VISIBLE);
+
+            new CountDownTimer(600, 600) {
+
+                @Override
+                public void onTick(long millisUntilFinished) { }
+
+                public void onFinish() {
+                    messageTextView.setVisibility(View.INVISIBLE);
+                }
+            }.start();
+        }
+    }
 
     public void endGame() {
 
+        new CountDownTimer(500, 500) {
+
+            @Override
+            public void onTick(long millisUntilFinished) { }
+
+            public void onFinish() {
+                messageTextView.setText("Finished!");
+                messageTextView.setVisibility(View.VISIBLE);
+                playAgainButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
+
+    public void onPlayAgainButtonPressed(View view) {
+        startGame();
     }
 
     @Override
